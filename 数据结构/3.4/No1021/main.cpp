0f95bfba0ee5a27a3fe6db8a5ea7f86653d86dea
cheng yiyang <qtcyy2005@gmail.com>
2025-03-25 11:07:45 +0800

    feat: 🎸 继续完成No1021

--- 数据结构/3.4/No1021/main.cpp
+++ 数据结构/3.4/No1021/main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 class Solution {
 public:
-  string removeOuterParentheses(string s) {}
+  string removeOuterParentheses(string s) {
+    string ans, st;
+    int n = s.length(), pos = 0;
+    for (int i = 0; i < n; i++) {
+      ans.push_back(s[i]);
+      if (st.size() && s[i] == ')' && st.back() == '(') {
+        st.pop_back();
+        if (!st.size()) {
+          ans.erase(pos, 1);
+          ans.pop_back();
+          pos = ans.size();
+        }
+      } else {
+        st.push_back(s[i]);
+      }
+    }
+    return ans;
+  }
 };
 
 signed main() {
