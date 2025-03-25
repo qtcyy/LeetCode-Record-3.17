00ff5baadcc1e13a2b54c136ffe88827f6e516d0
cheng yiyang <qtcyy2005@gmail.com>
2025-03-25 15:05:22 +0800

    feat: 🎸 完成No239
    滑动窗口 单调队列模板题

--- 数据结构/chapter4/4.4/No239/main.cpp
+++ 数据结构/chapter4/4.4/No239/main.cpp
@@ -1,10 +1,28 @@
+#include <deque>
 #include <iostream>
 #include <vector>
 using namespace std;
 
 class Solution {
 public:
-  vector<int> maxSlidingWindow(vector<int> &nums, int k) {}
+  vector<int> maxSlidingWindow(vector<int> &nums, int k) {
+    deque<int> q;
+    vector<int> ans;
+    int n = nums.size();
+    for (int i = 0; i < n; i++) {
+      while (!q.empty() && nums[q.back()] <= nums[i]) {
+        q.pop_back();
+      }
+      q.push_back(i);
+      while (!q.empty() && q.front() <= i - k) {
+        q.pop_front();
+      }
+      if (i >= k - 1) {
+        ans.push_back(nums[q.front()]);
+      }
+    }
+    return ans;
+  }
 };
 
 signed main() {
