20886e8c3346f41f6ce022dc39967e9687ef87b8
cheng yiyang <qtcyy2005@gmail.com>
2025-03-25 19:27:55 +0800

    feat: 🎸 完成No1046

--- 数据结构/chapter5/5.1/No1046/main.cpp
+++ 数据结构/chapter5/5.1/No1046/main.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
+#include <queue>
 #include <vector>
 using namespace std;
 
 class Solution {
 public:
-  int lastStoneWeight(vector<int> &stones) {}
+  int lastStoneWeight(vector<int> &stones) {
+    priority_queue<int> pq;
+    for (auto &x : stones) {
+      pq.push(x);
+    }
+    while (pq.size() >= 2) {
+      int x = pq.top();
+      pq.pop();
+      int y = pq.top();
+      pq.pop();
+      if (x == y) {
+        continue;
+      } else {
+        pq.push(x - y);
+      }
+    }
+    if (pq.empty()) {
+      return 0;
+    }
+    return pq.top();
+  }
 };
 
 signed main() {
-  vector<int> stones = {2, 7, 4, 1, 8, 1};
+  vector<int> stones = {2, 2};
   Solution sol = Solution();
   cout << sol.lastStoneWeight(stones) << endl;
 
