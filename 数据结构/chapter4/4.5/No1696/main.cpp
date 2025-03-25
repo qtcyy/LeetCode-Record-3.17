1300a859bfd28a42b2d8ea2af664278c1bf2bc1f
cheng yiyang <qtcyy2005@gmail.com>
2025-03-25 18:03:39 +0800

    feat: 🎸 完成No1696

--- 数据结构/chapter4/4.5/No1696/main.cpp
+++ 数据结构/chapter4/4.5/No1696/main.cpp
@@ -1,10 +1,28 @@
+#include <deque>
 #include <iostream>
 #include <vector>
 using namespace std;
 
 class Solution {
 public:
-  int maxResult(vector<int> &nums, int k) {}
+  int maxResult(vector<int> &nums, int k) {
+    int n = nums.size();
+    vector<int> f(n, 0);
+    f[0] = nums[0];
+    deque<int> deq;
+    for (int i = 1; i < n; i++) {
+      while (!deq.empty() && f[deq.back()] < f[i - 1]) {
+        deq.pop_back();
+      }
+      deq.push_back(i - 1);
+      while (!deq.empty() && deq.front() < i - k) {
+        deq.pop_front();
+      }
+
+      f[i] = f[deq.front()] + nums[i];
+    }
+    return f[n - 1];
+  }
 };
 
 signed main() {
