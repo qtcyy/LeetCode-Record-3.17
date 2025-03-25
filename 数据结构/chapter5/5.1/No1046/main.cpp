f6ef1af2ef08cdacb2ba4db76940673acc4e522f
cheng yiyang <qtcyy2005@gmail.com>
2025-03-25 19:22:47 +0800

    feat: 🎸 开始第五部分 优先队列
    添加No1046

---
+++ 数据结构/chapter5/5.1/No1046/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  int lastStoneWeight(vector<int> &stones) {}
+};
+
+signed main() {
+  vector<int> stones = {2, 7, 4, 1, 8, 1};
+  Solution sol = Solution();
+  cout << sol.lastStoneWeight(stones) << endl;
+
+  return 0;
+}
---
+++ 数据结构/chapter5/5.1/readme.md
@@ -0,0 +1,5 @@
+# 5.1 基础
+
+**题目列表**
+
+- [1046](https://leetcode.cn/problems/last-stone-weight/description/). 最后一块石头的重量
