fccac9af13f617a6695468cb971faeab7af33caa
cheng yiyang <qtcyy2005@gmail.com>
2025-03-25 13:49:30 +0800

    feat: 🎸 完成No1006

--- 数据结构/3.3/No1006/main.cpp
+++ 数据结构/3.3/No1006/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <stack>
 using namespace std;
 
 class Solution {
 public:
-  int clumsy(int n) {}
+  int clumsy(int n) {
+    stack<int> st;
+    st.push(n);
+    n--;
+    int cnt = 0;
+
+    while (n > 0) {
+      if (cnt % 4 == 0) {
+        st.top() *= n;
+      } else if (cnt % 4 == 1) {
+        st.top() /= n;
+      } else if (cnt % 4 == 2) {
+        st.push(n);
+      } else {
+        st.push(-n);
+      }
+      cnt++;
+      n--;
+    }
+    int ans = 0;
+    while (!st.empty()) {
+      ans += st.top();
+      st.pop();
+    }
+
+    return ans;
+  }
 };
 
 signed main() {
