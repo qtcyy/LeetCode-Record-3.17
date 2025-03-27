8b58f107a3e60eaee4202500e2393ab24faa018b
cheng yiyang <qtcyy2005@gmail.com>
2025-03-27 11:19:33 +0800

    feat: 🎸 保留7.3的部分题目 开始7.4

---
+++ 数据结构/chapter7/7.4/No1562/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  int findLatestStep(vector<int> &arr, int m) {}
+};
+
+signed main() {
+  vector<int> arr = {3, 5, 1, 2, 4};
+  int m = 1;
+}
---
+++ 数据结构/chapter7/7.4/readme.md
@@ -0,0 +1,5 @@
+# 7.4 数组上的并查集
+
+**题目列表**
+
+- [1562. 查找大小为 M 的最新分组](https://leetcode.cn/problems/find-latest-group-of-size-m/description/)
