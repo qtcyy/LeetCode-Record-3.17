39d8d11144bb6d10ecc0f69c33bd30d4b69ce214
cheng yiyang <qtcyy2005@gmail.com>
2025-03-27 16:52:33 +0800

    feat: 🎸 完成No1456

--- 滑动窗口与双指针/chapter1/1.1/No1456/main.cpp
+++ 滑动窗口与双指针/chapter1/1.1/No1456/main.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
 #include <string>
+#include <unordered_set>
 using namespace std;
 
 class Solution {
 public:
-  int maxVowels(string s, int k) {}
+  int maxVowels(string s, int k) {
+    int n = s.length();
+    int ans = 0, cur = 0;
+    unordered_set<char> un = {'a', 'e', 'i', 'o', 'u'};
+    for (int i = 0; i < k; i++) {
+      if (un.contains(s[i])) {
+        ++cur;
+      }
+    }
+    ans = cur;
+
+    int i = 0, j = k - 1;
+    while (j + 1 < n) {
+      if (un.contains(s[j + 1])) {
+        ++cur;
+      }
+      if (un.contains(s[i])) {
+        --cur;
+      }
+      ans = max(ans, cur);
+      ++i;
+      ++j;
+    }
+    return ans;
+  }
 };
 
 signed main() {
