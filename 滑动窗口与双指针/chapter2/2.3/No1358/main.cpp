65b7f0b6f705a3123427984333a8f5630d4bad98
cheng yiyang <qtcyy2005@gmail.com>
2025-03-28 20:00:14 +0800

    feat: 🎸 完成No1358

--- 滑动窗口与双指针/chapter2/2.3/No1358/main.cpp
+++ 滑动窗口与双指针/chapter2/2.3/No1358/main.cpp
@@ -4,7 +4,20 @@ using namespace std;
 
 class Solution {
 public:
-  int numberOfSubstrings(string s) {}
+  int numberOfSubstrings(string s) {
+    int n = s.length();
+    int cnt[3]{};
+    int left = 0, ans = 0;
+    for (int i = 0; i < n; i++) {
+      ++cnt[s[i] - 'a'];
+      while (cnt[0] && cnt[1] && cnt[2]) {
+        --cnt[s[left++] - 'a'];
+      }
+      ans += left;
+    }
+
+    return ans;
+  }
 };
 
 signed main() {
