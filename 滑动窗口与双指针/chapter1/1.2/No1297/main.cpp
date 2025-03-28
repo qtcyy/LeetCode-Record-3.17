10012b8e895943eddea8d2baaa961d2115949f77
cheng yiyang <qtcyy2005@gmail.com>
2025-03-28 08:38:22 +0800

    feat: 🎸 完成No1297

---
+++ 滑动窗口与双指针/chapter1/1.2/No1297/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  int maxFreq(string s, int maxLetters, int minSize, int maxSize) {
+    int n = s.length(), ans = 0;
+    auto process = [&](const int len) {
+      unordered_map<char, int> cnt;
+      unordered_map<string, int> sum;
+      for (int i = 0; i <= n; i++) {
+        if (i < len) {
+          ++cnt[s[i]];
+        } else {
+          if (cnt.size() <= maxLetters) {
+            ++sum[s.substr(i - len, len)];
+          }
+          if (i == n) {
+            break;
+          }
+          ++cnt[s[i]];
+          if (!--cnt[s[i - len]]) {
+            cnt.erase(s[i - len]);
+          }
+        }
+      }
+      for (auto &&[str, val] : sum) {
+        cout << str << endl;
+        ans = max(ans, val);
+      }
+    };
+    for (int len = minSize; len <= maxSize; len++) {
+      process(len);
+    }
+
+    return ans;
+  }
+};
+
+signed main() {
+  string s = "aababcaab";
+  int maxLetters = 2, minSize = 3, maxSize = 4;
+  Solution sol;
+  cout << sol.maxFreq(s, maxLetters, minSize, maxSize) << endl;
+
+  return 0;
+}
--- 滑动窗口与双指针/chapter1/1.2/readme.md
+++ 滑动窗口与双指针/chapter1/1.2/readme.md
@@ -4,3 +4,4 @@
 
 - [3439. 重新安排会议得到最多空余时间 I](https://leetcode.cn/problems/reschedule-meetings-for-maximum-free-time-i/description/)
 - [2134. 最少交换次数来组合所有的 1 II](https://leetcode.cn/problems/minimum-swaps-to-group-all-1s-together-ii/description/)
+- [1297. 子串的最大出现次数](https://leetcode.cn/problems/maximum-number-of-occurrences-of-a-substring/)
