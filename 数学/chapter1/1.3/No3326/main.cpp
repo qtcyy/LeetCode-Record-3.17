814bd5089e352998f3ec95f72406ba457ef3ed3e
cheng yiyang <qtcyy2005@gmail.com>
2025-04-01 08:29:25 +0800

    feat: 🎸 完成No3326

--- 数学/chapter1/1.3/No3326/main.cpp
+++ 数学/chapter1/1.3/No3326/main.cpp
@@ -2,9 +2,38 @@
 #include <vector>
 using namespace std;
 
+const int N = 1e6 + 5;
+
+vector<int> LFP(N);
+
+auto calc = [] {
+  for (int i = 2; i < N; i++) {
+    if (!LFP[i]) {
+      for (int j = i; j < N; j += i) {
+        if (!LFP[j])
+          LFP[j] = i;
+      }
+    }
+  }
+  return 0;
+}();
+
 class Solution {
 public:
-  int minOperations(vector<int> &nums) {}
+  int minOperations(vector<int> &nums) {
+    int n = nums.size();
+    int ans = 0;
+    for (int i = n - 2; i >= 0; i--) {
+      if (nums[i] > nums[i + 1]) {
+        nums[i] = LFP[nums[i]];
+        if (nums[i] > nums[i + 1]) {
+          return -1;
+        }
+        ++ans;
+      }
+    }
+    return ans;
+  }
 };
 
 signed main() {
