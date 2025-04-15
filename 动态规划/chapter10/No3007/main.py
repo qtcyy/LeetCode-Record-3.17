d14fc5b4e0cf9ae9ae15f3c0bec6b9122872f1f3
cheng yiyang <qtcyy2005@gmail.com>
2025-04-15 08:26:46 +0800

    fix: 🐛 修改No3007

--- 动态规划/chapter10/No3007/main.py
+++ 动态规划/chapter10/No3007/main.py
@@ -4,27 +4,23 @@ from functools import cache
 class Solution:
     def findMaximumNumber(self, k: int, x: int) -> int:
         def check(mid: int) -> bool:
-            m = mid.bit_length() - 1
+            m = mid.bit_length()
 
             @cache
-            def dfs(i: int, cnt: int, is_limit: bool, is_num: bool) -> int:
-                if i < 0:
-                    return cnt if is_num else 0
+            def dfs(i: int, cnt: int, is_limit: bool) -> int:
+                if i == 0:
+                    return cnt
+                up = mid >> (i - 1) & 1 if is_limit else 1
                 res = 0
-                up = mid >> i & 1 if is_limit else 1
-                if not is_num:
-                    res = dfs(i - 1, cnt, is_limit and up == 0, False)
-                if up == 1:
+                for d in range(up + 1):
                     res += dfs(
-                        i - 1, cnt + ((i + 1) % x == 0), is_limit and up == 1, True
+                        i - 1, cnt + (d == 1 and i % x == 0), is_limit and d == up
                     )
                 return res
 
-            val = dfs(m, 0, True, False)
-            print(val)
-            return val <= k
+            return dfs(m, 0, True) <= k
 
-        l, r = -1, k + 1
+        l, r = -1, ((k + 1) << x) + 1
         while l + 1 < r:
             mid = (l + r) >> 1
             if check(mid):
